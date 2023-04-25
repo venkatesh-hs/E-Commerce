0c6a1ece898dee53bfa45fbf4090b324e1bde6b4
Venkatesh HS <hsvenkatesh10@gmail.com>
2023-04-25 20:34:33 +0530

    Cross platform implementation

--- src/main/java/com/revival/inventory/book/controller/BookController.java
+++ src/main/java/com/revival/inventory/book/controller/BookController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/books")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BookController {
 
     @Autowired
--- src/main/java/com/revival/inventory/book/controller/UserController.java
+++ src/main/java/com/revival/inventory/book/controller/UserController.java
@@ -4,7 +4,6 @@ import com.revival.inventory.book.entities.User;
 import com.revival.inventory.book.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +12,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     private ResponseEntity<User> createUser(@RequestBody User user) {
         userService.createUser(user);
         return ResponseEntity
